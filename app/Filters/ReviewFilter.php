b1257b213677618d41a1b41b91bc4975988fdcb1
MariamEhabShaaban <ehabm0817@gmail.com>
2026-06-13 21:14:31 +0300

    api-filter-query-mariam-ehab

--- app/Filters/ReviewFilter.php
+++ app/Filters/ReviewFilter.php
@@ -6,6 +6,16 @@ use App\Filters\QueryFilter;
 
 class ReviewFilter extends QueryFilter
 {
+
+   public function user_id($value)
+    {
+        $this->builder->where('user_id', $value);
+    }
+
+    public function meal_id($value)
+    {
+        $this->builder->where('meal_id', $value);
+    }
     public function rating($value)
     {
         $this->builder->where('rating', $value);
