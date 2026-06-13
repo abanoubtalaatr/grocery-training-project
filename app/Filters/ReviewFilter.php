fae96d6d14275388c1dcf9e5ff9faf7acf61731c
MahaSalehh <mahamsaleh279@gmail.com>
2026-06-13 21:37:28 +0300

    api filter query

---
+++ app/Filters/ReviewFilter.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Filters;
+
+use App\Filters\QueryFilter;
+
+class ReviewFilter extends QueryFilter
+{
+
+    public function user_id($value)
+    {
+        $this->builder->where('user_id', $value);
+    }
+
+    public function meal_id($value)
+    {
+        $this->builder->where('meal_id', $value);
+    }
+    public function rating($value)
+    {
+        $this->builder->where('rating', $value);
+    }
+
+    public function is_approved($value)
+    {
+        $this->builder->where('is_approved', $value);
+    }
+
+    
+    
+}
