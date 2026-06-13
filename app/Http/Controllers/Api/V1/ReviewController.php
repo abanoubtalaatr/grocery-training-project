ef5194a6714e46055f5c87e85d2bb69265c7f9b1
‘ElhamSamir151196’ <elham.samir.cairo@gmail.com>
2026-06-13 22:21:20 +0300

    filtered-api-test-commit

---
+++ app/Http/Controllers/Api/V1/ReviewController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    public function index(Request $request)
+    {
+        $reviews = Review::query()->filter($request)->get();
+        return response()->json($reviews);
+    }
+}
