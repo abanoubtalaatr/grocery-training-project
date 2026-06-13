0a6dc45726cd97ed067641200b0df58fe1260f3b
MariamEhabShaaban <ehabm0817@gmail.com>
2026-06-13 21:08:59 +0300

    api-filter-query-mariam-ehab

---
+++ app/Filters/ReviewFilter.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Filters;
+
+use App\Filters\QueryFilter;
+
+class ReviewFilter extends QueryFilter
+{
+    public function rating($value)
+    {
+        $this->builder->where('rating', $value);
+    }
+
+    public function is_approved($value)
+    {
+        $this->builder->where('is_approved', $value);
+    }
+
+    
+    
+}
