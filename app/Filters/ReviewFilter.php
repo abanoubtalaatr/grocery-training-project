2d99f082350fc44a69c9b1d3d69903af9f41578a
mohamed ibrahim <mohamed.ibrahim.blaqiti@gmail.com>
2026-06-13 21:43:03 +0300

    add filters

---
+++ app/Filters/ReviewFilter.php
@@ -0,0 +1,15 @@
+<?php
+use App\Filters\QueryFilter;
+
+class ReviewFilter extends QueryFilter
+{
+        public function user_id($value)
+        {
+            $this->builder->where('user_id', $value);
+        }
+
+        public function meal_id($value)
+        {
+            $this->builder->where('meal_id', $value);
+        }
+}
