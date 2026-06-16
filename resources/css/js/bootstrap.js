aeb890f72e2cb5401d6d102bcd22f66fac001897
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-06-16 20:43:53 +0300

    refactor: remove unused JavaScript assets and initialize empty CSS file

--- resources/css/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-// Import the Bootstrap CSS library
-import "bootstrap";
-
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-import axios from "axios";
-window.axios = axios;
-
-window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
